44ad5e8083dab1676fd7d6890b39ffcba16873c8
felipebelarmino <epilefgomes1@gmail.com>
2021-06-12 17:01:17 -0300

    Db Connection string Added

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -1,9 +1,15 @@
+using Dot_Net_Core_API_with_JWT.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Dot_Net_Core_API_with_JWT.Data
 {
     public class DataContext : DbContext
     {
-        
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters { get; set; }
     }
 }
